6540be9331676b1e1669c2b5b01a26f63c37f76b
Haim Aflalo <kodkodhaim@gmail.com>
2026-01-19 13:05:10 +0200

    correction

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ import { Router } from './router/routes.js';
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/user', function (req, res) {
-  res.sendFile('C:/Users/INTERNET/Desktop/FormsBasic/client/register.html');
+  res.sendFile(
+    'C:/Users/INTERNET/Desktop/Html/FormsBasic/client/register.html',
+  );
 });
+
 app.use('/user', Router);
 
 app.listen(3000, () => {
--- services/functions.js
+++ services/functions.js
@@ -3,17 +3,16 @@ import fs from 'fs';
 async function addUser(data) {
   const users = await getUsers();
   console.log(users);
-
   users.push(data);
   return await fs.promises.writeFile(
-    'C:/Users/INTERNET/Desktop/FormsBasic/data/data.json',
+    'C:/Users/INTERNET/Desktop/Html/FormsBasic/data/data.json',
     JSON.stringify(users),
   );
 }
 
 async function getUsers() {
   const data = await fs.promises.readFile(
-    'C:/Users/INTERNET/Desktop/FormsBasic/data/data.json',
+    'C:/Users/INTERNET/Desktop/Html/FormsBasic/data/data.json',
     'utf-8',
   );
   return JSON.parse(data);
